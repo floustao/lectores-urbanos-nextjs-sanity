8abcd2a20ebcb5019d99667d258a48c9320f1dd3
floustao <florestanlefevre@gmail.com>
2024-03-01 19:14:31 -0800

    Add redirect to webapp from studio

--- StudioComponents/CustomNavBar.tsx
+++ StudioComponents/CustomNavBar.tsx
@@ -1,4 +1,6 @@
+import { Link } from '@chakra-ui/react'
 import { Card, Flex, Stack, Text } from '@sanity/ui'
+import NextLink from 'next/link'
 import { NavbarProps, useWorkspace } from 'sanity'
 
 import UpdateCompaniesButton from './UpdateCompaniesButton'
@@ -7,17 +9,21 @@ export function CustomNavBar(props: NavbarProps) {
   const { dataset } = useWorkspace()
 
   return (
-    <Card>
+    <Card padding={2}>
       <Stack>
-        <Flex justify="space-between" align="center">
-          <Card padding={4}>
-            <Text size={1}>
+        <Card tone="critical" padding={4}>
+          <Flex justify="space-between" align="center">
+            <Text size={2}>
               Using the <b>{dataset}</b> dataset
             </Text>
-          </Card>
 
-          <UpdateCompaniesButton />
-        </Flex>
+            <Link as={NextLink} href="/" color="white">
+              Go back to website
+            </Link>
+
+            <UpdateCompaniesButton />
+          </Flex>
+        </Card>
         {props.renderDefault(props)} {/* Render the default navbar */}
       </Stack>
     </Card>
