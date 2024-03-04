646d25ea657531aeb6e5626200f49a735a87bea1
floustao <florestanlefevre@gmail.com>
2024-03-03 17:03:01 -0800

    Rewrite updateCompanyBooks to ensure company gets next book in list instead of a random attribution

--- src/backend/sanityService.js
+++ src/backend/sanityService.js
@@ -1,32 +1,32 @@
-import { readToken, writeToken } from '~/lib/sanity.api'
+import { writeToken } from '~/lib/sanity.api'
 import { getClient } from '~/lib/sanity.client'
+
+// Closure mechanism that ensures step is incremented each time we trigger updateCompanyBooks
+const startingIndex = (() => {
+  let step = 0;
+  return () => step++;
+})();
 
 
 export async function updateCompanyBooks() {
   const client = getClient({ token: writeToken })
   try {
-    const nonDraftsCompanies = await client.fetch('*[_type == "company" && !(_id in path("drafts.**")) && !(_originalId in path("drafts.**"))]');
+    const allNonDraftsCompanies = await client.fetch('*[_type == "company" && !(_id in path("drafts.**")) && !(_originalId in path("drafts.**"))]');
+    const allNonDraftBooks = await client.fetch('*[_type == "book" && !(_id in path("drafts.**")) && !(_originalId in path("drafts.**"))]')
+    const step = startingIndex();
 
-    for (const company of nonDraftsCompanies) {
-      const newBookRef = await selectRandomBook();
+    for (let index = 0; index < allNonDraftsCompanies.length; index++) {
+      // Getting next book in the list of books
+      const bookIndex = (index + step) % allNonDraftBooks.length;
+      const book = {_type: 'reference', _ref: allNonDraftBooks[bookIndex]._id};
 
-      // Update the company in Sanity with the new book reference
-      await client.patch(company._id)
-        .set({ book: newBookRef })
+      // Associating company to next book in the list
+      await client.patch(allNonDraftsCompanies[index]._id)
+        .set({ book })
         .commit();
     }
-
-    console.log('Companies updated successfully');
   } catch (error) {
     console.error('Error updating companies:', error);
     throw error;
   }
-}
-
-async function selectRandomBook() {
-  const client = getClient({ token: readToken })
-  const books = await client.fetch('*[_type == "book"]');
-  const randomBookIndex = Math.floor(Math.random() * books.length);
-
-  return { _type: 'reference', _ref: books[randomBookIndex]._id };
 }
